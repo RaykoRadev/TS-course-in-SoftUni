cabfa3b78f862f03cb72c046088634dfb9eaba62
RaykoRadev <christmas898@gmail.com>
2025-05-21 17:11:31 +0300

    last 2 task of second topic

--- Exercise 2/05.Hotel Visit.ts
+++ Exercise 2/05.Hotel Visit.ts
@@ -16,25 +16,25 @@ interface sleep extends something {
     sleep(): void;
 }
 
-function visitFloor(floor: train | dine | sleep) {
-    switch (floor.number) {
-        case 1:
-            floor.train();
-            return;
-        case 2:
-            floor.dine();
-            return;
-        case 3:
-            floor.sleep();
-            return;
-    }
-}
+// function visitFloor(floor: train | dine | sleep) {
+//     switch (floor.number) {
+//         case 1:
+//             floor.train();
+//             return;
+//         case 2:
+//             floor.dine();
+//             return;
+//         case 3:
+//             floor.sleep();
+//             return;
+//     }
+// }
 
-visitFloor({ train() {}, number: 1, hallway: "A", pass: "Guest" });
-visitFloor({ dine() {}, number: 2, hallway: "A" });
-visitFloor({ sleep() {}, number: 3, hallway: "C" });
-visitFloor({ train() {}, number: 1, hallway: "C" });
-visitFloor({ train() {}, number: 1, hallway: "A" });
-visitFloor({ dine() {}, number: 2, hallway: "A", pass: "Guest" });
-visitFloor({ sleep() {}, number: 3, hallway: "A" });
-visitFloor({ dine() {}, number: 2, hallway: "C" });
+// visitFloor({ train() {}, number: 1, hallway: "A", pass: "Guest" });
+// visitFloor({ dine() {}, number: 2, hallway: "A" });
+// visitFloor({ sleep() {}, number: 3, hallway: "C" });
+// visitFloor({ train() {}, number: 1, hallway: "C" });
+// visitFloor({ train() {}, number: 1, hallway: "A" });
+// visitFloor({ dine() {}, number: 2, hallway: "A", pass: "Guest" });
+// visitFloor({ sleep() {}, number: 3, hallway: "A" });
+// visitFloor({ dine() {}, number: 2, hallway: "C" });
---
+++ Exercise 2/08.Extract Interfaces.ts
@@ -0,0 +1,51 @@
+type StatusType = "Logged" | "Started" | "InProgress" | "Done";
+
+interface Tasks {
+    status: StatusType;
+    title: string;
+    daysRequired: number;
+    assignedTo: Users | undefined;
+    changeStatus(newStatus: StatusType): void;
+}
+
+interface Users {
+    username: string;
+    signupDate: Date;
+}
+
+function assignTask(user: Users, task: Tasks) {
+    if (task.assignedTo == undefined) {
+        task.assignedTo = user;
+        console.log(`User ${user.username} assigned to task '${task.title}'`);
+    }
+}
+
+let user = {
+    username: "Margaret",
+    signupDate: new Date(2022, 1, 13),
+    passwordHash: "random",
+};
+let task1 = {
+    status: <"Logged" | "Started" | "InProgress" | "Done">"Logged",
+    title: "Need assistance",
+    daysRequired: 1,
+    assignedTo: undefined,
+    changeStatus(newStatus: "Logged" | "Started" | "InProgress" | "Done") {
+        this.status = newStatus;
+    },
+};
+
+let task2 = {
+    status: <"Logged" | "Started" | "InProgress" | "Done">"Done",
+    title: "Test",
+    daysRequired: 12,
+    assignedTo: undefined,
+    changeStatus(newStatus: "Logged" | "Started" | "InProgress" | "Done") {
+        this.status = newStatus;
+    },
+    moreProps: 300,
+    evenMore: "wow",
+};
+
+assignTask(user, task1);
+assignTask(user, task2);
