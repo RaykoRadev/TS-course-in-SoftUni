aaa23394b25c8e01c40150ed41f358139d2d4713
RaykoRadev <christmas898@gmail.com>
2025-05-21 13:55:30 +0300

    the begining of task 3

--- Exercise 2/03.Car Diagnostics.ts
+++ Exercise 2/03.Car Diagnostics.ts
@@ -1,3 +1,19 @@
+interface body {
+    material: string;
+    state: string;
+}
+
+interface tires {
+    airPresure: number;
+    condition: string;
+}
+
+interface engine {
+    horspowers: number;
+    oilDensity: number;
+}
+
+function solve(arg1: body, arg2: tires, arg3: engine) {}
 // { material: 'aluminum', state: 'scratched', partName: 'Car Body', runDiagnostics },
 // { airPressure: 30, condition: 'needs change', partName: 'Tires', runDiagnostics },
 // { horsepower: 300, oilDensity: 780, partName: 'Engine', runDiagnostics }
